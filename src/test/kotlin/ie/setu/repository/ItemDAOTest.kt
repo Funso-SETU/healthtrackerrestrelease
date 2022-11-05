115f081803f41371fbf4f6ee133ca6ce03289b26
Funso <20096225@mail.wit.ie>
2022-11-05 19:16:06 +0000

    Add this for my health tracker app after creating activity,activities, items, measurements classes and DB and deploying to Heroku

---
+++ src/test/kotlin/ie/setu/repository/ItemDAOTest.kt
@@ -0,0 +1,96 @@
+package ie.setu.repository
+
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import ie.setu.domain.Item
+import ie.setu.helpers.*
+import kotlin.test.assertEquals
+
+//retrieving some test data from Fixtures
+private val item1 = item.get(0)
+private val item2 = item.get(1)
+private val item3 = item.get(2)
+
+class ItemDAOTest {
+
+    companion object {
+        //Make a connection to a local, in memory H2 database.
+        @BeforeAll
+        @JvmStatic
+        internal fun setupInMemoryDatabaseConnection() {
+            Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
+        }
+    }
+
+    @Nested
+    inner class CreateItems {
+
+        @Test
+        fun `multiple items added to table can be retrieved successfully`() {
+            transaction {
+                //Arrange - create and populate tables with three items
+                val itemDAO = populateItemTable()
+                //Act & Assert
+                assertEquals(3, itemDAO.getAll().size)
+                assertEquals(item, itemDAO.getAllItems(item))
+
+            }
+        }
+    }
+
+    @Nested
+    inner class ReadItems {
+
+        @Test
+        fun `getting all items from a populated table returns all rows`() {
+            transaction {
+                //Arrange - create and populate tables with three users and three activities
+                val itemDAO = populateItemTable()
+                //Act & Assert
+                assertEquals(3, itemDAO.getAll().size)
+            }
+        }
+
+    }
+
+    @Nested
+    inner class UpdateItems {
+
+        @Test
+        fun `updating existing item in table results in successful update`() {
+            transaction {
+
+                val itemDAO = populateItemTable()
+
+                //Act & Assert
+                val item1updated = Item(id = 1, name = "Mango", unitprice = 322.0, quantity = 230, dateadded = DateTime.now(), userid = 1)
+                itemDAO.update(item1updated.id, item1updated)
+                assertEquals(item1updated, itemDAO.findById(1))
+            }
+        }
+    }
+
+    @Nested
+    inner class DeleteItems {
+
+
+        @Test
+        fun `deleting an existing activity (by id) in table results in record being deleted`() {
+            transaction {
+                val itemDAO = populateItemTable()
+
+                //Act & Assert
+                assertEquals(3, itemDAO.getAll().size)
+                itemDAO.delete(item2.id)
+                assertEquals(2, itemDAO.getAll().size)
+            }
+        }
+
+
+
+    }
+}
---
+++ src/test/kotlin/ie/setu/repository/MeasurementDAOTest.kt
@@ -0,0 +1,97 @@
+package ie.setu.repository
+
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import ie.setu.domain.Item
+import ie.setu.domain.Measurement
+import ie.setu.helpers.*
+import kotlin.test.assertEquals
+
+//retrieving some test data from Fixtures
+private val meast1 = measurements.get(0)
+private val meast2 = measurements.get(1)
+private val meast3 = measurements.get(2)
+
+class MeasurementDAOTest {
+
+    companion object {
+        //Make a connection to a local, in memory H2 database.
+        @BeforeAll
+        @JvmStatic
+        internal fun setupInMemoryDatabaseConnection() {
+            Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
+        }
+    }
+
+    @Nested
+    inner class CreateItems {
+
+        @Test
+        fun `multiple measurements added to table can be retrieved successfully`() {
+            transaction {
+                //Arrange - create and populate tables with three items
+                val measurementDAO = populateMeasurementTable()
+                //Act & Assert
+                assertEquals(3, measurementDAO.getAll().size)
+                assertEquals(measurements, measurementDAO.getAllMeasurements(measurements))
+
+            }
+        }
+    }
+
+    @Nested
+    inner class ReadItems {
+
+        @Test
+        fun `getting all measurements from a populated table returns all rows`() {
+            transaction {
+                //Arrange - create and populate tables with three users and three activities
+                val measurementDAO = populateMeasurementTable()
+                //Act & Assert
+                assertEquals(3, measurementDAO.getAll().size)
+            }
+        }
+
+    }
+
+    @Nested
+    inner class UpdateItems {
+
+        @Test
+        fun `updating existing measurement in table results in successful update`() {
+            transaction {
+
+                val measurementDAO = populateMeasurementTable()
+
+                //Act & Assert
+                val meast1updated = Measurement(id = 1, chest = 23.0, waist = 22.0, dateadded = DateTime.now(), userid = 1)
+                measurementDAO.update(meast1updated.id, meast1updated)
+                assertEquals(meast1updated, measurementDAO.findById(1))
+            }
+        }
+    }
+
+    @Nested
+    inner class DeleteItems {
+
+
+        @Test
+        fun `deleting an existing measurement (by id) in table results in record being deleted`() {
+            transaction {
+                val measurementDAO = populateMeasurementTable()
+
+                //Act & Assert
+                assertEquals(3, measurementDAO.getAll().size)
+                measurementDAO.delete(meast2.id)
+                assertEquals(2, measurementDAO.getAll().size)
+            }
+        }
+
+
+
+    }
+}
